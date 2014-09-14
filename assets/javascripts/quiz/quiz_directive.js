7d6f697ddac4fd226e9b97e54ec5453dd4ec3d03
Nichlos Incardone <nickincardone@gmail.com>
2014-09-13 20:22:55 -0400

    quiz now shows submit button only when all questions are answered

--- assets/javascripts/quiz/quiz_directive.js
+++ assets/javascripts/quiz/quiz_directive.js
@@ -1,11 +1,11 @@
 (function(angular) {
 
   var
-  definitions;
+    definitions;
 
   definitions = [
-  niQuiz
-  ]; 
+    niQuiz
+  ];
 
   angular.module('ni.Quiz').directive('niQuiz', definitions);
 
@@ -21,13 +21,50 @@
     };
 
     function createQuiz(scope, elm, attrs) {
+      //start quiz on first question
       scope.currentQuestion = 0;
+
+      //init user choice array to null to test for missing answer later
       scope.choice = [];
       for (var i = 0; i < scope.quiz.length; i++) {
         scope.choice[i] = null;
       }
 
+      //tracks if quiz is complete
+      scope.quizDone = false;
+
+      //tracks if all questions are answered
+      scope.quizAnswered = function() {
+        for (var i = 0; i < scope.choice.length; i++) {
+          if (scope.choice[i] == null) {
+            return false;
+          }
+        }
+        return true;
+      } 
+
+      scope.checkQuiz = function() {
+        for (var i = 0; i < scope.choice.length; i++) {
+          if (scope.choice[i] == null) {
+            scope.quizAnswered = false;
+            return;
+          }
+        }
+        scope.quizAnswered = true;
+      }
+
+      scope.next = function() {
+        scope.currentQuestion++;
+      }
+
+      scope.previous = function() {
+        scope.currentQuestion--;
+      }
+
+      scope.submitQuiz = function() {
+        scope.quizDone = true;
+      }
 
     }
   }
-}) (angular);
+})(angular);
